3dd5ce60a646bf9a7bc72c1480377d845710dc84
sarganio <sarganio@post.bgu.ac.il>
2025-03-26 10:39:57 +0200

    style50 fixes

--- traffic.py
+++ traffic.py
@@ -77,7 +77,7 @@ def get_model():
     model = tf.keras.models.Sequential([tf.keras.layers.Lambda(
         lambda x: tf.image.rgb_to_grayscale(x), input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),])
     model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu',
-                                    input_shape=(IMG_WIDTH, IMG_HEIGHT, 1)))
+                                     input_shape=(IMG_WIDTH, IMG_HEIGHT, 1)))
     model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=2))
     # Flatten units
     model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
